1d2fefd01af27ce7fbf05e51b822e9b33f2dd48a
manis-9219 <ping2manish92@gmail.com>
2025-06-04 20:37:04 +0530

    changing the lambda code to print key and file size

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -2,7 +2,8 @@ import json
 def lambda_handler(event, context):
     '''This is lambda handler accepts 
     "event -> trigger, context -> lambda runtime context'''
-    print('event', event)
+    print('key', event['Records'][0]['s3']['object']['key'])
+    print('size in MB', event['Records'][0]['s3']['object']['size']/(1024*1024))
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from lambda')
