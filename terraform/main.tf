146b6d05bade4f0e634adbb38356ad21393997de
manis-9219 <ping2manish92@gmail.com>
2025-06-06 15:35:04 +0530

    commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,7 +45,7 @@ resource "aws_s3_bucket_notification" "notify" {
   lambda_function {
     lambda_function_arn = aws_lambda_function.lambda.arn
     events              = ["s3:ObjectCreated:*"]
-    filter_prefix = ""    # root only
+    filter_prefix = "Upload/"    # root only
   }
 
   // Ensure Lambda permission is created before notification
