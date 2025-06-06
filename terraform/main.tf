fc9bfc18ee8d1c5ceb505e91fa0d4d122c682104
manis-9219 <ping2manish92@gmail.com>
2025-06-06 13:43:53 +0530

    fixing?

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,18 +88,18 @@ resource "aws_iam_role_policy" "lambda_s3_access" {
     Version = "2012-10-17",
     Statement = [
       {
+        Effect = "Allow",
         Action = [
           "s3:ListBucket",
           "s3:GetObject",
           "s3:PutObject"
-          ]
+        ],
+        Resource = [
+          aws_s3_bucket.bucket.arn,
+          "${aws_s3_bucket.bucket.arn}/*"
+        ]
       }
-    ],
-    Resource= [
-      aws_s3_bucket.bucket.arn,
-      "${aws_s3_bucket.bucket.arn}/*"
     ]
-
   })
 }
 
